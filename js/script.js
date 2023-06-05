51fd135f7505b259c57b918743acc46f47025f0e
Sardor Akbarovich <sardor.cm.1287@gmail.com>
2023-06-05 13:51:16 +0500

    comlate the homework

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// let alrt = alert("Salom")
+// let prmt = prompt("Ismingizni kiriting")
+// let cnfrm = confirm("Siz maktabda o'qiysizmi ?")
+
+// console.log(prmt + " ismli bola maktabda o'qiydimi " + cnfrm)
+// let a= 1
+// let b = 1
+// let c = ++a
+// let d = b++ 
+// console.log()
+// console.log(d)
+
+
+let prmt = prompt("birinchi sonni kiriting: ")
+let prnp = prompt("ishorani kiriting: ")
+let prnt = prompt("ikkinchi sonni kiriting: ")
+if (prnp == "/"){
+    console.log(prmt / prnt)
+}else if(prnp == "*"){
+    console.log(prmt * prnt)
+}else if(prnp == "+"){
+    console.log((-prmt) + (-prnt)) 
+}else if(prnp == "-"){
+    console.log("" - (prmt - prnt))
+}
+
+
+
+
